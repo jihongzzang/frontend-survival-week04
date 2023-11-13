bb439edc8e4799806acaf901adcf763bb6c8a39a
jujihong <jujihong2@gmail.com>
2023-11-13 20:29:13 +0900

    feat: style 복구

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,6 +1,8 @@
 import FilterableMenuContainer from './components/FilterableMenuConainer';
 
 import useInit from './hooks/useInit';
+import './styles/reset.css';
+import './styles/style.css';
 
 export default function App() {
   const { restaurntsData } = useInit();
